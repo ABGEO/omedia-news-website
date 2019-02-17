52bae9cb1b7602e2f3afd6e1c615a1ccd7ed8a11
ABGEO <takalandzet@gmail.com>
2019-02-17 16:30:02 +0400

    Added main news page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\News;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
     public function index()
     {
+        $doctrine = $this->getDoctrine();
+        $news = $doctrine->getRepository(News::class)->findAll();
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'news' => $news,
         ]);
     }
 }
