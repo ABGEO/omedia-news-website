190acdbc43bd8990f62aa872a167867423463a55
ABGEO <takalandzet@gmail.com>
2019-02-17 20:34:24 +0400

    Send subscription form

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\News;
+use App\Entity\Subscriber;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends AbstractController
 {
@@ -34,5 +36,26 @@ class DefaultController extends AbstractController
         return $this->render('default/single_news.html.twig', [
             'news' => $news,
         ]);
+    }
+
+    public function subscribeOnNews(Request $request)
+    {
+        $request = $request->request;
+
+        $token = $request->get('token');
+        $email = $request->get('email');
+
+        if ($this->isCsrfTokenValid('subscribe-on-news', $token)) {
+
+            $subscriber = new Subscriber();
+            $subscriber->setEmail($email);
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($subscriber);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('index');
     }
 }
