26d5591a17ff977365a58e7d727f5c922f7d7803
ABGEO <takalandzet@gmail.com>
2019-02-17 15:17:07 +0400

    Added users page and user deleting feature in admin-panel

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,15 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
     public function index()
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        return $this->render('admin/index.html.twig');
+    }
+
+    /**
+     * Show all users
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function users()
+    {
+        $doctrine = $this->getDoctrine();
+        $users = $doctrine->getRepository(User::class)->findAll();
+
+        return $this->render('admin/users.html.twig', [
+            'users' => $users
         ]);
+    }
+
+
+    /**
+     * Remove user from DB
+     *
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function removeUser($id)
+    {
+        $doctrine = $this->getDoctrine();
+        $em = $doctrine->getManager();
+
+        $user = $doctrine->getRepository(User::class)->find($id);
+
+        if ($user) {
+            $em->remove($user);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_users');
     }
 }
